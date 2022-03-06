6fa2a878f7ffde2e329d1a3b4db1feedb4ff4e26
huang <2120268953@qq.com>
2022-03-06 14:36:17 +0800

    h

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+// 引入createApp函数，创建对应的应用，产生应用的实例对象
 import { createApp } from 'vue'
+// 引用app组件，所有组件的父组件
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 创建app应用返回对应的实例对象,调用mount方法进行挂载
 createApp(App).use(store).use(router).mount('#app')
